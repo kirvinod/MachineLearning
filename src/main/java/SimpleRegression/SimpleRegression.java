41ce20a46a66db9b9a5a832c9f2801cb9a3911c5
vkumar <vinod.kumar@kennedy.ox.ac.uk>
2019-04-17 17:20:25 +0100

    Added datasets

--- src/main/java/SimpleRegression/SimpleRegression.java
+++ src/main/java/SimpleRegression/SimpleRegression.java
@@ -34,33 +34,73 @@ public class SimpleRegression {
 
         double r = this.getCorrelationCoefficient();
 
-        double n = this.variables.size();
+        double Sx = 0;
+        double Sy = 0;
 
-        System.out.println(n);
-        double xSum = 0;
-        double ySum = 0;
 
-        double xySum = 0;
 
-        for (Variable variable: this.variables) {
-            //System.out.println(variable.getX());
-            xSum += variable.getX();
-            ySum += variable.getY();
-            xySum +=  variable.getX() * variable.getY();
-        }
 
-//        System.out.println(xSum);
-//        System.out.println(ySum);
-       // System.out.println((7*(xySum)) - (xSum * ySum));
+        System.out.println(r);
 
-        //System.out.println(xySum);
+
         return 0;
     }
 
     private double getCorrelationCoefficient() {
 
-        return 0;
+        double n = this.variables.size();
+        double xSum = 0;
+        double ySum = 0;
+        double xxSum = 0;
+        double yySum = 0;
+        double xySum = 0;
+
+        for (Variable variable: this.variables) {
+            xSum += variable.getX();
+            ySum += variable.getY();
+            xxSum += variable.getX() * variable.getX();
+            yySum += variable.getY() * variable.getY();
+            xySum +=  variable.getX() * variable.getY();
+        }
+
+
+
     }
+
+//    private double getCorrelationCoefficientTest() {
+//
+//        double n = this.variables.size();
+//
+//        double xSum = 0;
+//        double ySum = 0;
+//        double xxSum = 0;
+//        double yySum = 0;
+//
+//        double xySum = 0;
+//
+//        for (Variable variable: this.variables) {
+//            xSum += variable.getX();
+//            ySum += variable.getY();
+//            xxSum += variable.getX() * variable.getX();
+//            yySum += variable.getY() * variable.getY();
+//            xySum +=  variable.getX() * variable.getY();
+//        }
+//
+//        // co-variation
+//        double cov = xySum / n - xSum * ySum / n / n;
+//
+//        // standard error of x
+//        double sigmax = Math.sqrt(xxSum / n -  xSum * xSum / n / n);
+//
+//        // standard error of y
+//        double sigmay = Math.sqrt(yySum / n -  ySum * ySum / n / n);
+//
+//        // correlation is just a normalized co-variation
+//        double r = ( cov / sigmax / sigmay);
+//
+//        return r;
+//
+//    }
 
     private double getYIntercept() {
         return 0;
